62850c874083e5eaa541e75085c4798d4a10641f
ianllamas <ianllamas@gmail.com>
2019-02-22 21:56:01 -0500

    commit

---
+++ Python/windows gui example.py
@@ -0,0 +1,23 @@
+from tkinter import *
+
+window=Tk()
+
+l1 = label(windows, text="Title")
+l1.grid(row=0,column=0)
+
+l1=label(window, text="Author")
+lid.grid(row=0,column=2)
+
+l1=label(window,text="Year")
+l1.grid(row=1,column=0)
+
+l1=label(window,text="ISBN")
+l1.grid(row=1,column=2)
+
+title_text=StringVar()
+el=Entry(window,textvariable=author_text)
+el.grid(row=0,column=1)
+
+title_text=StringVar()
+el=Entry(window,textvariable=author_text)
+el.grid(row=0,column=1)
