ac2408afb30a651b2cc4d7f09156090bb8b4db6b
ianllamas <ianllamas@gmail.com>
2019-02-22 22:34:35 -0500

    commit

--- Python/windows gui example.py
+++ Python/windows gui example.py
@@ -15,9 +15,31 @@ l1=label(window,text="ISBN")
 l1.grid(row=1,column=2)
 
 title_text=StringVar()
-el=Entry(window,textvariable=author_text)
-el.grid(row=0,column=1)
+e1=Entry(window,textvariable=title_text)
+e1.grid(row=0,column=1)
 
-title_text=StringVar()
-el=Entry(window,textvariable=author_text)
-el.grid(row=0,column=1)
+author_text=StringVar()
+e2=Entry(window,textvariable=author_text)
+e2.grid(row=0,column=3)
+
+year_text=StringVar()
+e3=Entry(window,textvariable=year_text)
+e3.grid(row=1,column=1)
+
+isbn_text=StringVar()
+e4=Entry(window,textvariable=isbn_text)
+e4.grid(row=1,column=3)
+
+list1=ListBox(windows, height=6, width=35)
+list1.grid(row=2, column=0, columnspan=2)
+
+sb1 = scrollbar(window)
+sb1.grid(row=2,column=2,rowspan=6)
+
+list1.configure(yscrollcommand=sb1.set)
+sb1.configure(command=list1.yview)
+
+b1=Button(window,text="View All", width=12)
+b1.grid(row=2,column=3)
+
+windows.mainloop()
