b86e54f125e27545b81cabaaf93741697b472d3c
Ian Llamas <ianllamas@gmail.com>
2019-10-08 00:33:53 -0400

    yes

--- Python/windows gui example.py
+++ Python/windows gui example.py
@@ -1,9 +1,9 @@
-from tkinter import Tk, Label, Button
+from tkinter import*
 
 cal = Tk()
 cal.title("Calculator")
 operator=""
-text_Input = StringVar()
+text_Input =StringVar()
 
 txtDisplay = Entry (cal, font =('arial', 20,'bold'), textvariable=text_Input, bd=30, insertwidth=4, bg="powder blue"
 , justify='right').grid(columnspan=4)
