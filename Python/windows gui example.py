b3a8720d54362a4762971246b25b71e5545a5782
Ian Llamas <ianllamas@gmail.com>
2019-10-08 00:29:45 -0400

    yes

--- Python/windows gui example.py
+++ Python/windows gui example.py
@@ -1,63 +1,14 @@
 from tkinter import*
-import random
-import time;
 
-window=Tk()
+cal = Tk()
+cal.title("Calculator")
+operator=""
+text_Input = StringVar()
 
-l1 = label(windows, text="Title")
-l1.grid(row=0,column=0)
+txtDisplay = Entry(cal, font =('arial', 20,'bold'), textvariable=text_Input, bd=30, insertwidth=4, bg="powder blue"
+, justify='right').grid(columnspan=4)
 
-l1=label(window, text="Author")
-lid.grid(row=0,column=2)
+btn7=Button(cal,padx=16,bd=8,fg="black",font=('arial',20,'bold'), text="7").grid(row=1,column=0)
 
-l1=label(window,text="Year")
-l1.grid(row=1,column=0)
-
-l1=label(window,text="ISBN")
-l1.grid(row=1,column=2)
-
-title_text=StringVar()
-e1=Entry(window,textvariable=title_text)
-e1.grid(row=0,column=1)
-
-author_text=StringVar()
-e2=Entry(window,textvariable=author_text)
-e2.grid(row=0,column=3)
-
-year_text=StringVar()
-e3=Entry(window,textvariable=year_text)
-e3.grid(row=1,column=1)
-
-isbn_text=StringVar()
-e4=Entry(window,textvariable=isbn_text)
-e4.grid(row=1,column=3)
-
-list1=ListBox(windows, height=6, width=35)
-list1.grid(row=2, column=0,rowspan=6, columnspan=2)
-
-sb1 = scrollbar(window)
-sb1.grid(row=2,column=2,rowspan=6)
-
-list1.configure(yscrollcommand=sb1.set)
-sb1.configure(command=list1.yview)
-
-b1=Button(window,text="View All", width=12)
-b1.grid(row=2,column=3)
-
-b1=Button(window,text="Search Entry", width=12)
-b1.grid(row=3,column=3)
-
-b1=Button(window,text="Add Entry", width=12)
-b1.grid(row=4,column=3)
-
-b1=Button(window,text="Update Selected", width=12)
-b1.grid(row=5,column=3)
-
-b1=Button(window,text="Delete Selected", width=12)
-b1.grid(row=6,column=3)
-
-b1=Button(window,text="Close", width=12)
-b1.grid(row=7,column=3)
-
-windows.mainloop()
+cal.mainloop()
 
